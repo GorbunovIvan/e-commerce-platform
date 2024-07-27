632c8b8992ef0247430bb71837ec6cc99fbc3730
GorbunovIvan <110302058+GorbunovIvan@users.noreply.github.com>
2024-07-27 09:36:06 +0300

    product-service - N+1 problem is resolved via EntityGraph

--- product-service/src/main/java/org/example/repository/ProductRepository.java
+++ product-service/src/main/java/org/example/repository/ProductRepository.java
@@ -1,11 +1,26 @@
 package org.example.repository;
 
 import org.example.model.Product;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
+
+    @Override
+    @EntityGraph(attributePaths = { "category", "user" })
+    @NonNull
+    List<Product> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = { "category", "user" })
+    @NonNull
+    <S extends Product> List<S> findAll(@NonNull Example<S> example);
+
+    @EntityGraph(attributePaths = { "category", "user" })
     List<Product> findAllByIdIn(Collection<Long> ids);
 }
